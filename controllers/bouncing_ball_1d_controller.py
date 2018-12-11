3d64c34c40c3c29f995d7568c2fe003f9651c254
Clark <clarkz@seas.upenn.edu>
2018-12-11 12:42:54 -0500

    minor 1d controller chjanges

--- controllers/bouncing_ball_1d_controller.py
+++ controllers/bouncing_ball_1d_controller.py
@@ -5,9 +5,9 @@ import math
 
 
 class BouncingBall1DController(object):
-    def __init__(self, coeff_of_restitution):
+    def __init__(self, coeff_of_restitution, desired_height):
         self.coeff_of_restitution = coeff_of_restitution
-        self.desired_height = 10.
+        self.desired_height = desired_height
 
     def get_control(self, x):
         grav = -9.81
@@ -15,14 +15,12 @@ class BouncingBall1DController(object):
         ball_vel_at_impact = x[1] + grav*time_until_impact
 
         desired_vel_after_impact = np.sqrt(2*-grav*self.desired_height)
-
         desired_paddle_vel = (desired_vel_after_impact + ball_vel_at_impact) / (self.coeff_of_restitution + 1)
 
 
         # fit a quadratic path to the controller
         tau = time_until_impact
         coeffs = np.zeros(3) # from highest ordered coeff to lowest
-
         A = np.array([[tau**2, tau], [2*tau, 1.]])
         b = np.array([[0., desired_paddle_vel]]).T - np.array([[x[2], 0]]).T
         sol = np.linalg.solve(A, b).squeeze()
@@ -30,6 +28,7 @@ class BouncingBall1DController(object):
         coeffs[1] = sol[1]
         coeffs[2] = x[2]
 
+        # return the current desired velocity
         current_vel = coeffs[1]
 
         action = np.array([current_vel])
--- controllers/tests/example_bouncing_ball_1d.py
+++ controllers/tests/example_bouncing_ball_1d.py
@@ -6,18 +6,19 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     restitution = 0.9
+    desired_height = 7
 
     model = BouncingBall1DModel(max_vel=float('inf'), drag_coeff=0., bounce_restitution=restitution)
-    controller = BouncingBall1DController(restitution)
+    controller = BouncingBall1DController(restitution, desired_height)
 
-    dt = 0.05
+    dt = 0.01
 
-    T = 1000
+    T = 2000
     state_hist = np.zeros((T, 3))
     control_hist = np.zeros((T, 1))
 
     state = np.array([5., 0., -2])
-    for t in range(1000):
+    for t in range(T):
         action = controller.get_control(state)
         new_state = model.get_next_state(state, action, dt)
 
@@ -32,24 +33,23 @@ if __name__ == '__main__':
 
     time_array = [dt*i for i in range(T)]
 
-    plt.subplot(4, 1, 1)
-    plt.plot(time_array, state_hist[:, 0])
+    plt.subplot(3, 1, 1)
+    plt.plot(time_array, state_hist[:, 2], label='paddle')
+    plt.plot(time_array, state_hist[:, 0], label='ball')
+    plt.plot(time_array, desired_height * np.ones(len(time_array)), label='desired')
+    plt.legend()
     plt.xlabel("time (s)")
-    plt.ylabel("ball pos (m)")
+    plt.ylabel("pos (m)")
 
-    plt.subplot(4, 1, 2)
+
+
+    plt.subplot(3, 1, 2)
     plt.plot(time_array, state_hist[:, 1])
     plt.xlabel("time (s)")
     plt.ylabel("ball vel (m/s)")
 
 
-    plt.subplot(4, 1, 3)
-    plt.plot(time_array, state_hist[:, 2])
-    plt.xlabel("time (s)")
-    plt.ylabel("paddle pos (m)")
-
-
-    plt.subplot(4, 1, 4)
+    plt.subplot(3, 1, 3)
     plt.plot(time_array, control_hist[:, 0])
     plt.xlabel("time (s)")
     plt.ylabel("paddle vel (m/s)")
